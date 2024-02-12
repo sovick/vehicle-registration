424795ed4e4c7103fbd7930057c29ad81f3bc824
dev-sk <dev.sovick@gmail.com>
2024-02-13 05:04:04 +0530

    validation added, postman collection added

--- src/app/routes/vehicle/index.ts
+++ src/app/routes/vehicle/index.ts
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import VehicleController from "../../controllers/vehicle";
+import InputValidation from "../../utils/middlewares/validations";
 
 const vehicleControllerObj = new VehicleController();
+const validator = new InputValidation();
 
 const vehicleRouter = Router();
 
-vehicleRouter.post('/add-new',vehicleControllerObj.addVehicle);
+vehicleRouter.post('/add-new',validator.validateVehicleRegister,vehicleControllerObj.addVehicle);
 
 vehicleRouter.get('/lists',vehicleControllerObj.getAllVehicles);
 
-vehicleRouter.get('/list/:vin',vehicleControllerObj.getVehicle);
+vehicleRouter.get('/list/:vin',validator.validategetVehicle,vehicleControllerObj.getVehicle);
 
-vehicleRouter.put('/update/:vin',vehicleControllerObj.updateVehicleDetails);
+vehicleRouter.put('/update/:vin',validator.validateUpdateVehicleDetails,vehicleControllerObj.updateVehicleDetails);
 
-vehicleRouter.delete('/delete/:vin',vehicleControllerObj.deleteVehicleDetails);
+vehicleRouter.delete('/delete/:vin',validator.validateDeleteVehicle,vehicleControllerObj.deleteVehicleDetails);
 
 export default vehicleRouter;
--- src/app/utils/middlewares/validations/index.ts
+++ src/app/utils/middlewares/validations/index.ts
@@ -30,5 +30,118 @@ export default class InputValidation {
 
     }
 
+    validateVehicleRegister(req : Request, res : Response,next : NextFunction){
+
+        try{
+
+            const schema = z.object({
+                body : z.object({
+                    data : z.object({
+                        manufacturerName : z.string(),
+                        modelName : z.string(),
+                        year : z.number(),
+                        fuelType : z.string(),
+                        firstName : z.string(),
+                        lastName : z.string(),
+                        email : z.string().email(),
+                        address : z.string(),
+                        VIN : z.string(),
+                        color : z.string(),
+                        purchaseDate : z.string(),
+                        purchasePrice : z.number()
+                    })
+                })
+            })
+
+            schema.parse({
+                body : req.body
+            });
+
+            next();
+
+        }catch(e){
+            return res.status(400).json({
+                status : 'error',
+                message : 'Bad payload'
+            })
+        }
+    }
+
+    validategetVehicle(req : Request, res : Response,next : NextFunction){
+        try{
+
+            const schema = z.object({
+                params : z.object({
+                    vin : z.string()
+                })
+            })
+
+            schema.parse({
+                params : req.params 
+            });
+
+            next();
+
+        }catch(e){
+            return res.status(400).json({
+                status : 'error',
+                message : 'Bad payload'
+            })
+        }
+
+    }
+
+    validateUpdateVehicleDetails(req : Request, res : Response,next : NextFunction){
+        try{
+
+            const schema = z.object({
+                body : z.object({
+                    data : z.object({
+                        color : z.string().optional(),
+                        purchaseDate : z.string().optional(),
+                        purchasePrice : z.number().optional()
+                    })
+                })
+            })
+
+            schema.parse({
+                body : req.body
+            });
+
+            next();
+
+        }catch(e){
+            return res.status(400).json({
+                status : 'error',
+                message : 'Bad payload'
+            })
+        }
+
+    }
+
+    validateDeleteVehicle(req : Request, res : Response,next : NextFunction){
+        try{
+
+            const schema = z.object({
+                params : z.object({
+                    vin : z.string()
+                })
+            })
+
+            schema.parse({
+                params : req.params 
+            });
+
+            next();
+
+        }catch(e){
+            return res.status(400).json({
+                status : 'error',
+                message : 'Bad payload'
+            })
+        }
+
+    }
+
 
 }
